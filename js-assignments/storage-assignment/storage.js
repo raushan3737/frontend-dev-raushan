85837201f97498d83b8cc8e581668712a4cae419
Raushan Kumar Gupta <raushan.gupta@zemosolabs.com>
2023-03-06 13:51:14 +0530

    Modified: Adding the button for decrement the Storage score

--- js-assignments/storage-assignment/storage.js
+++ js-assignments/storage-assignment/storage.js
@@ -5,19 +5,19 @@ Submit the github link with your code
 
 */
 
-// Accessing the button 
+// Accessing the button
 const incBtn = document.getElementById("incBtn");
+const decBtn = document.getElementById("decBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-// Set the text of HTML according to the local & session storage we have. And storing in variable to increment 
+// Set the text of HTML according to the local & session storage we have. And storing in variable to increment
 let localText = (document.getElementById("localScoreTxt").textContent =
   localStorage.getItem("localScoreTxt"));
 
 let sessionText = (document.getElementById("sessionScoreTxt").textContent =
   sessionStorage.getItem("sessionScoreTxt"));
 
-
-// Incrementing the storage 
+// Incrementing the storage
 incBtn.addEventListener("click", () => {
   // Incrementing by 1
   localText++;
@@ -27,19 +27,36 @@ incBtn.addEventListener("click", () => {
   localStorage.setItem("localScoreTxt", localText);
   sessionStorage.setItem("sessionScoreTxt", sessionText);
 
-  // Set the text of HTML according to the incremented value. 
+  // Set the text of HTML according to the incremented value.
   document.getElementById("localScoreTxt").textContent = localText;
   document.getElementById("sessionScoreTxt").textContent = sessionText;
 });
 
+// Decrementing the storage
+decBtn.addEventListener("click", () => {
+  // If local & session score exist then, Decrement by 1 because, Negative score doesn't sense
+  if (localText) {
+    localText--;
+  }
+  if (sessionText) {
+    sessionText--;
+  }
 
-// Reset the storage 
-resetBtn.addEventListener('click',()=>{
-  localText =0 ; 
-  sessionText =0 ; 
+  // Set the decremented value in storage
+  localStorage.setItem("localScoreTxt", localText);
+  sessionStorage.setItem("sessionScoreTxt", sessionText);
+
+  // Set the text of HTML according to the decremented value.
+  document.getElementById("localScoreTxt").textContent = localText;
+  document.getElementById("sessionScoreTxt").textContent = sessionText;
+});
+
+// Reset the storage
+resetBtn.addEventListener("click", () => {
+  localText = 0;
+  sessionText = 0;
   localStorage.setItem("localScoreTxt", localText);
   sessionStorage.setItem("sessionScoreTxt", sessionText);
   document.getElementById("localScoreTxt").textContent = localText;
   document.getElementById("sessionScoreTxt").textContent = sessionText;
-
-} )
+});
